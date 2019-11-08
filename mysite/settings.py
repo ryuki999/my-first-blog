d51e96c75c33f0740bfb3d47dc223ed58ab019c2
ryuki999 <ryuki999.1624@gmail.com>
2019-11-09 00:21:13 +0900

    Change css

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #BASE_DIRはdjangogirls/までのフォルダ
-STATIC_ROOT = os.path.join(BASE_DIR, 'blog/templates/static')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATIC_ROOT = '/blog/templates/static'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'blog/templates/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
